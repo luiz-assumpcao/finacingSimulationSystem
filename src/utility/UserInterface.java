fd4cff480c2f0675486040f9b6f0e2ee7df09e73
Luiz <luizrafael.apneto@gmail.com>
2025-08-12 00:52:15 -0300

    Method for displaying error messages created.

--- src/utility/UserInterface.java
+++ src/utility/UserInterface.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class UserInterface {
     Scanner scanner = new Scanner(System.in);
 
+    private void printError(String message) {
+        System.out.println("\u001B[31m" + message + "\u001B[0m");
+    }
+
     public Double getPropertyValue() {
         double propertyValue;
         do {
             System.out.print("Enter the property value: ");
             propertyValue = Double.parseDouble(scanner.nextLine());
             if (propertyValue < 0.00) {
-                System.out.println("\u001B[31mThe value can't be negative! Try again.\u001B[0m");
+                printError("The value can't be negative! Try again.");
             } else if (propertyValue < 30000.00) {
-                System.out.println("\u001B[31mThe value can't be too low! Try again.\u001B[0m");
+                printError("The value can't be too low! Try again.");
             } else if (propertyValue > 60000000.00) {
-                System.out.println("\u001B[31mThe value can't be too high! Try again.\u001B[0m");
+                printError("The value can't be too high! Try again.");
             }
         } while (propertyValue < 30000.00 || propertyValue > 60000000.00);
         return propertyValue;
@@ -27,11 +31,11 @@ public class UserInterface {
             System.out.print("Enter the financing term: ");
             financingTerm = Integer.parseInt(scanner.nextLine());
             if (financingTerm < 0) {
-                System.out.println("\u001B[31mThe value can't be negative! Try again.\u001B[0m");
+                printError("The value can't be negative! Try again.");
             } else if (financingTerm < 5) {
-                System.out.println("\u001B[31mThe value can't be too low! Try again.\u001B[0m");
+                printError("The value can't be too low! Try again.");
             } else if (financingTerm > 50) {
-                System.out.println("\u001B[31mThe value can't be too high! Try again.\u001B[0m");
+                printError("The value can't be too high! Try again.");
             }
         } while (financingTerm < 5 || financingTerm > 50);
         return financingTerm;
@@ -43,10 +47,10 @@ public class UserInterface {
             System.out.print("Enter the annual interest rate: ");
             annualInterestRate = Double.parseDouble(scanner.nextLine());
             if (annualInterestRate < 0.00) {
-                System.out.println("\u001B[31mThe value can't be negative! Try again.\u001B[0m");
+                printError("The value can't be negative! Try again.");
             }
             if (annualInterestRate > 100.00) {
-                System.out.println("\u001B[31mThe value can't be too high! Try again.\u001B[0m");
+                printError("The value can't be too high! Try again.");
             }
         } while (annualInterestRate < 0.00 || annualInterestRate > 100.00);
         return annualInterestRate;
